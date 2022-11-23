6d3c16b98d8df41bbdee59a3c0344f84f17afc20
Techmj <mrenaaljain@gmail.com>
2022-11-22 23:54:56 -0500

    driveGearRatio and angleGearRatio added

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,8 +24,8 @@ public final class Constants {
         public static final double openLoopRamp = 0.25;
         public static final double closedLoopRamp = 0.0;
 
-        public static final double driveGearRatio = (5.14 / 1.0); // 5.14:1
-        public static final double angleGearRatio = (12.8 / 1.0); // 12.8:1
+        public static final double driveGearRatio = (6.75 / 1.0); // 6.75:1
+        public static final double angleGearRatio = ((150 / 7) / 1.0); // 150/7:1
 
         public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
                 new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
