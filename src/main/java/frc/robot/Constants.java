bb1dce1b9f32c6883fa23070c2e77d664346b02c
Techmj <mrenaaljain@gmail.com>
2022-12-04 13:33:10 -0500

    set wheel base and track width and wheel diameter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,8 +15,8 @@ public final class Constants {
         public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
 
         /* Drivetrain Constants */
-        public static final double trackWidth = Units.inchesToMeters(21.73);
-        public static final double wheelBase = Units.inchesToMeters(21.73);
+        public static final double trackWidth = Units.inchesToMeters(16.5);
+        public static final double wheelBase = Units.inchesToMeters(19.5);
         public static final double wheelDiameter = Units.inchesToMeters(4.0);
         public static final double wheelCircumference = wheelDiameter * Math.PI;
 
