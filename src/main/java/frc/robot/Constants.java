e1bb60c488bb5a9087036368209f272dd8a6d089
Techmj <mrenaaljain@gmail.com>
2023-03-12 19:08:22 -0400

    we'll be back :/

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,7 +18,7 @@ public final class Constants {
                 // public static final int restServoAngle = 10;
                 // public static final int extendServoAngle = 25;
                  public static final int restServoAngle = 10;
-                public static final int extendServoAngle = 35;
+                public static final int extendServoAngle = 45;
                 public static final int thruBoreDIO = 0;
                 public static final double tiltkP = 7.5;
                 public static final double tiltkD = 0.01;
