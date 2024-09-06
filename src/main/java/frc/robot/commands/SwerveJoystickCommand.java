a3640e217d626221045f717effab2732378e318a
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2024-09-06 19:25:22 -0400

    remove importa in SwerveJoystickCommand

--- src/main/java/frc/robot/commands/SwerveJoystickCommand.java
+++ src/main/java/frc/robot/commands/SwerveJoystickCommand.java
@@ -9,9 +9,7 @@ import java.util.function.Supplier;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Robot;
 import frc.robot.RobotContainer;
 import frc.robot.Constants.SwerveConstants;
 import frc.robot.subsystems.SwerveSubsystem;
