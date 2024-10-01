c9d1ff30a3bd63db714f69b0072350df04b9f47c
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2024-10-01 10:40:36 -0400

    Fix conversion factors

--- src/main/java/frc/robot/subsystems/SwerveBase.java
+++ src/main/java/frc/robot/subsystems/SwerveBase.java
@@ -77,11 +77,17 @@ public class SwerveBase extends SubsystemBase {
     backLeft.initRotationOffset();
     backRight.initRotationOffset();
 
-    // inversions -- need to figure these out
+    // drive motor inversions -- offsets mess with these sometimes
     frontLeft.getDriveMotor().setInverted(true);
     frontRight.getDriveMotor().setInverted(false);
     backLeft.getDriveMotor().setInverted(false);
     backRight.getDriveMotor().setInverted(false);
+
+    // rotation motor inversions -- all or nothing situation
+    frontLeft.getRotationMotor().setInverted(false);
+    frontRight.getRotationMotor().setInverted(false);
+    backLeft.getRotationMotor().setInverted(false);
+    backRight.getRotationMotor().setInverted(false);
 
     // reset encoders upon each start
     frontLeft.resetEncoders();
@@ -89,31 +95,7 @@ public class SwerveBase extends SubsystemBase {
     backLeft.resetEncoders();
     backRight.resetEncoders();
 
-    AutoBuilder.configureHolonomic(
-      this::getPose, // robot pose supplier
-      this::resetOdometry, // method to reset odometry (will be called if auto has a starting pose)
-      this::getRobotRelativeSpeeds, // ChassisSpeeds supplier (must be robot relative)
-      this::driveRobotRelative, // method that will drive the robot given robot relative ChassisSpeeds
-      new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-        new PIDConstants(0, 0, 0), // Translation PID constants
-        new PIDConstants(0, 0, 0), // Rotation PID constants
-        SwerveConstants.maxMetersPerSecond, // Max module speed in m/s
-        SwerveConstants.driveBaseRadius, // drive base radius in meters, distance from robot center to furthest module
-        new ReplanningConfig()// Default path replanning config, see the API for options
-      ),
-      () -> {
-        // Boolean supplier that controls when the path will be mirrored for the red alliance
-        // This will flip the path being followed to the red side of the field.
-        // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-
-        var alliance = DriverStation.getAlliance();
-        if (alliance.isPresent()) {
-          return alliance.get() == DriverStation.Alliance.Red;
-        }
-        return false;
-      },
-      this
-    );
+    configureAutoBuilder();
   }
 
   public Pose2d getPose() {
@@ -226,6 +208,34 @@ public class SwerveBase extends SubsystemBase {
     } else {
       RobotContainer.fieldRelativeStatus = true;
     }
+  }
+
+  public void configureAutoBuilder() {
+    AutoBuilder.configureHolonomic(
+      this::getPose, // robot pose supplier
+      this::resetOdometry, // method to reset odometry (will be called if auto has a starting pose)
+      this::getRobotRelativeSpeeds, // ChassisSpeeds supplier (must be robot relative)
+      this::driveRobotRelative, // method that will drive the robot given robot relative ChassisSpeeds
+      new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+        new PIDConstants(0, 0, 0), // Translation PID constants
+        new PIDConstants(0, 0, 0), // Rotation PID constants
+        SwerveConstants.maxMetersPerSecond, // Max module speed in m/s
+        SwerveConstants.driveBaseRadius, // drive base radius in meters, distance from robot center to furthest module
+        new ReplanningConfig()// Default path replanning config, see the API for options
+      ),
+      () -> {
+        // Boolean supplier that controls when the path will be mirrored for the red alliance
+        // This will flip the path being followed to the red side of the field.
+        // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+          return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+      },
+      this
+    );
   }
 
   @Override
